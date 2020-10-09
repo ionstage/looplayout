7339995643fae491cd00a0ad1870c456c66c3ea8
nasitra <nasitra@users.noreply.github.com>
2020-10-09 23:30:21 +0900

    Set CSS styles of background color in JS code

--- docs/js/app.js
+++ docs/js/app.js
@@ -8,6 +8,10 @@ export class App {
   }
 
   init () {
+    const style = document.body.style
+    style.backgroundAttachment = 'fixed'
+    style.backgroundImage = "url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxkZWZzPjxwYXR0ZXJuIGlkPSJncmlkIiB3aWR0aD0iMTIwIiBoZWlnaHQ9IjEyMCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHJlY3Qgd2lkdGg9IjEyMCIgaGVpZ2h0PSIxMjAiIGZpbGw9IiNmYWZhZmEiIC8+PHBhdGggZD0iTSAxMjAgMCBMIDAgMCAwIDEyMCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjNzU3NTc1IiBzdHJva2Utd2lkdGg9IjEiIC8+PC9wYXR0ZXJuPjwvZGVmcz48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2dyaWQpIiAvPjwvc3ZnPg==')"
+    style.backgroundSize = '120px 120px'
     this.scrollHandler.enable()
   }
 
