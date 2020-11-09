5f75bfe89d375ddb5f2ce274988ecab6a090d2de
nasitra <nasitra@users.noreply.github.com>
2020-11-09 21:34:25 +0900

    Make js code for content

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,24 +1,15 @@
+import { Content } from './content.js';
 import { ScrollHandler } from './scroll-handler.js';
 
 class App {
   constructor() {
-    this.scrollHandler = new ScrollHandler({ onscroll: this.scroll.bind(this) });
-    this.x = 0;
-    this.y = 0;
+    this.content = new Content();
+    this.scrollHandler = new ScrollHandler({ onscroll: this.content.scroll.bind(this.content) });
   }
 
   init() {
-    const style = document.body.style;
-    style.backgroundAttachment = 'fixed';
-    style.backgroundImage = "url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxkZWZzPjxwYXR0ZXJuIGlkPSJncmlkIiB3aWR0aD0iMTIwIiBoZWlnaHQ9IjEyMCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHJlY3Qgd2lkdGg9IjEyMCIgaGVpZ2h0PSIxMjAiIGZpbGw9IiNmYWZhZmEiIC8+PHBhdGggZD0iTSAxMjAgMCBMIDAgMCAwIDEyMCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjNzU3NTc1IiBzdHJva2Utd2lkdGg9IjEiIC8+PC9wYXR0ZXJuPjwvZGVmcz48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2dyaWQpIiAvPjwvc3ZnPg==')";
-    style.backgroundSize = '120px 120px';
+    this.content.init();
     this.scrollHandler.enable();
-  }
-
-  scroll(dx, dy) {
-    this.x += dx;
-    this.y += dy;
-    document.body.style.backgroundPosition = -this.x + 'px ' + -this.y + 'px';
   }
 }
 
