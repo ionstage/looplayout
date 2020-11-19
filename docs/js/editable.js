12301c80786aaf88974683138f2608bc484a7657
nasitra <nasitra@users.noreply.github.com>
2020-11-19 23:27:45 +0900

    Set position and size of 'editable' element

--- docs/js/editable.js
+++ docs/js/editable.js
@@ -1,5 +1,16 @@
 const template = document.createElement('template');
 
+template.innerHTML = `
+  <style>
+    :host {
+      bottom: 0;
+      height: 48px;
+      position: fixed;
+      width: 100%;
+    }
+  </style>
+`;
+
 class Editable extends HTMLElement {
   constructor() {
     super();
